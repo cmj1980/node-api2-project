9833f2f66aeb62e0e71b69c4d1f488a714065520
cmj1980 <dagoclmt@gmail.com>
2022-06-07 21:51:34 -0700

    server file setup

--- api/server.js
+++ api/server.js
@@ -1,9 +1,19 @@
 // implement your server here
+// require your posts router and connect it here
 const express = require('express');
 
 const server = express();
 
 server.use(express.json());
-// require your posts router and connect it here
+
+const postsRouter = require('./posts/posts-router');
+server.use('/api/posts', postsRouter);
+
+server.get('/', (req, res) => {
+    res.send(`
+    <h1>Posts Api</h1>
+    <p>Welcome to the Posts API</p>
+    `)
+})
 
 module.exports = server
